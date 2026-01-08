f2b2cb6df02bdefae98a11e2575e3ddcbeb1a966
Athoillah21 <muhammadathoillah62@gmail.com>
2026-01-08 10:40:45 +0700

    Enable DEBUG and revert to working Vercel config

--- api/index.py
+++
@@ -1,9 +0,0 @@
-"""
-Vercel serverless function entry point for Django WSGI.
-"""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tuning_buddy.settings')
-
-app = get_wsgi_application()
